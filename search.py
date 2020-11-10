03a27ec7c8fed57251f001696ba1a247998ff26b
miarao <miaraomer@gmail.com>
2020-11-10 15:11:05 +0200

    Finished Pacman AI project

---
+++ search.py
@@ -0,0 +1,220 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+#
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    """
+    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
+    sequence of moves will be incorrect, so only use this for tinyMaze.
+    """
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem):
+    """
+    DFS explores the graph using stack to create LIFO based exploration,
+    path by path.
+    """
+
+    from util import Stack
+    #init stack to manage frontier
+    frontier = Stack()
+    startState = problem.getStartState()
+    explored = []
+
+    frontier.push((startState, [], 0))
+
+    while not frontier.isEmpty():
+        currentState, actions, costs = frontier.pop()
+
+        if currentState not in explored:
+
+            explored.append(currentState)
+            goalStateReached = problem.isGoalState(currentState)
+
+            if goalStateReached:
+                return actions
+
+            for state, action, cost in problem.getSuccessors(currentState):
+                if not state in explored:
+                    allActions = actions + [action]
+                    frontier.push((state, allActions, cost))
+    raise Exception("search has failed")
+
+def breadthFirstSearch(problem):
+    """
+    BFS explore the graph using queue to create a FIFO based exploration
+    the edges are expanded level by level
+    """
+
+    from util import Queue
+    #init queue to manage frontier
+    frontier = Queue()
+    startState = problem.getStartState()
+    explored = []
+
+    frontier.push((startState, [], 0))
+
+    while not frontier.isEmpty():
+        currentState, actions, costs = frontier.pop()
+
+        if currentState not in explored:
+
+            explored.append(currentState)
+            goalStateReached = problem.isGoalState(currentState)
+
+            if goalStateReached:
+                return actions
+
+            for state, action, cost in problem.getSuccessors(currentState):
+                if not state in explored:
+                    allActions = actions + [action]
+                    frontier.push((state, allActions, cost))
+    raise Exception("search has failed")
+
+def uniformCostSearch(problem):
+    """
+    UCS uses cost based prioritized Queue, to explore the graph similarly to BFS,
+    while considering what path is the least costly.
+    """
+
+
+    from util import PriorityQueue
+    #init priority queue to manage frontier
+    frontier = PriorityQueue()
+
+    startState = problem.getStartState()
+    explored = []
+
+    frontier.push((startState, [], 0), 0)
+
+    while not frontier.isEmpty():
+        currentState, actions, costs = frontier.pop()
+
+        if currentState not in explored:
+
+            explored.append(currentState)
+            goalStateReached = problem.isGoalState(currentState)
+
+            if goalStateReached:
+                return actions
+
+            for state, action, cost in problem.getSuccessors(currentState):
+                if not state in explored:
+                    totalCost = costs + cost
+                    allActions = actions + [action]
+                    frontier.push((state, allActions, totalCost), totalCost)
+    raise Exception("search has failed")
+
+
+def nullHeuristic(state, problem=None):
+    """
+    A heuristic function estimates the cost from the current state to the nearest
+    goal in the provided SearchProblem.  This heuristic is trivial.
+    """
+    return 0
+
+
+def aStarSearch(problem, heuristic=nullHeuristic):
+    """
+    A* uses cost based prioritized Queue, to explore the graph similarly to BFS,
+    while considering  combination of cost (like in UCS) and heuristic (consistent as it's
+    a graph search)
+    """
+    from util import PriorityQueue
+    # init priority queue to manage frontier
+    frontier = PriorityQueue()
+
+    startState = problem.getStartState()
+    explored = []
+
+    frontier.push((startState, [], 0), 0)
+
+    while not frontier.isEmpty():
+        currentState, actions, costs = frontier.pop()
+
+        if currentState not in explored:
+
+            explored.append(currentState)
+            goalStateReached = problem.isGoalState(currentState)
+
+            if goalStateReached:
+                return actions
+
+            for state, action, cost in problem.getSuccessors(currentState):
+                if state not in explored:
+                    # total cost is the sum of costs and the heuristic assessment
+                    totalCost = costs + cost + heuristic(state, problem)
+                    allActions = actions + [action]
+                    frontier.push((state, allActions, totalCost), totalCost)
+    raise Exception("search has failed")
+
+
+
+# Abbreviations
+bfs = breadthFirstSearch
+dfs = depthFirstSearch
+astar = aStarSearch
+ucs = uniformCostSearch
